a3e1ec78ee60b260a9e49f105e428aee118c2d22
Jadon <Jadonzufall@gmail.com>
2021-07-06 11:09:00 -0400

    Reconfigured Battle class
    When creating classes you want to created an init function that way you do something like
    mybattle = Battle('sword', 10, 'Bandit', enemy_hp=10)
    mybattle.start_battle()
    
    This allows you to actually make use of the classes functionality instead of basically using it as storage for a function.

--- battle.py
+++ battle.py
@@ -1,20 +1,34 @@
 import random
 
 class Battle:
-	base_health = 100
-	def fight(equipped, hit_chance, enemy_name="Monster", enemy_hp=10):
-		battle = True
-		# While the battle is going on, well, battle goes on
-		while battle:
-			a = int(input("What to do?\n1. Fight\n2. Run"))
+	def __init__(equipped, hit_chance, enemy_name="Monster", enemy_hp=10):
+		self.equipped = equipped
+		self.hit_chance = hit_chance
+		self.enemy_name = enemy_name
+		self.enemy_hp = enemy_hp
+	
+	def start_battle(self):
+		while True:
+			while True:
+				try:
+					a = int(input('What to do?\n1. Fight\n2. Run'))
+					if a not in range(1, 3):
+						raise TypeError		# This is just to call the except statement
+					else:
+						break
+				except TypeError:
+					print('Please select a valid input')
+		
 			if a == 1:
 				chance = random.random()
-				if chance > (int(hit_chance)/100):
-					weapon = equipped["weapon"]
-					print(f"Hit for {weapon[1]}!\n")
-					if int(weapon[1]) > int(enemy_hp):
-						print(f"{enemy_name} has been defeated!")
-						battle = False
-				else:
-					print("Argh! A miss!")
+				if chance > int(self.hit_chance) / 100:
+					weapon = self.equipped['weapon']
+					print('Hit for {}!\n'.format(weapon[1]))
+					if int(weapon[1]) > int(self.enemy_hp):
+						print('{} has been defeated!'.format(self.enemy_name))
+						break
+					else:
+						print('Argh! A miss!')
+					
+			
 # class Boss:
