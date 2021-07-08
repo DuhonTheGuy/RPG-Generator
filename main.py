5887b5b26817b279695279c8c6820f8865f1381f
Jadon <jadonzufall@gmail.com>
2021-07-08 01:51:42 -0400

    Updated start_menu

--- main.py
+++ main.py
@@ -5,64 +5,91 @@ import sys
 
 # Function to allow backtracking to the menu after a cancelled "from path".
 def start_menu():
+	# Main loop for start menu
 	while True:
-		try:
-			option = int(input("What do you want to do?\n1. List projects in current path\n2. Open path\n3. Exit\n"))
-			break
-		except ValueError:
-			print('Please select a valid option')
-			continue
-	
-	reader = Reader()
+		# Option loop for menu selection
+		while True:
+			try:
+				option = int(input("What do you want to do?\n1. List projects in current path\n2. Open path\n3. Exit\n"))
+				break
+			except ValueError:
+				print('Please select a valid option')
+				continue
+		
+		reader = Reader()
 
-	# Lists projects in current path.  (still a WIP)
-	if option == 1:
-		path = os.path.join(sys.path[0], "projects\\")
+		# Lists projects in current path.  (still a WIP)
+		if option == 1:
+			path = os.path.join(sys.path[0], "projects\\")
+			files = os.listdir(path)
 
-		files = os.listdir(path)
-		print(f"Current directory: {path}")
-		print("---------------------------------------------------------------")
-		for count, file in enumerate(files):
-			print(f"{count + 1}. {file}")
-		a = int(input("Which one do you want to access? "))
-		print(files)
-		result = path + files[a - 1]
-		#reader.read(result)
-		reader.read(result)
-	
-	# Opens specific file path.
-	elif option == 2:
-		# Check if specified path exists.
-		pathChecking = True
-		# While a valid path is not provided, ask until one is or the user cancels the operation.
-		while pathChecking:
-			path = input("Surely! What is the path?\n")
-			if os.path.exists(path):
-				#reader.read(path)
-				# End the checking loop.
-				pathChecking = False
-				reader.read(path)
-			else:
-				print("No such path exists!")
-				option = input("Try again? (1. No / 2. Yes)\n")
+			print(f"Current directory: {path}")
+			print("---------------------------------------------------------------")
+
+			for count, file in enumerate(files):
+				print(f"{count + 1}. {file}")
+			
+			a = int(input("Which one do you want to access? "))
+			print(files)
+
+			result = path + files[a - 1]
+			reader.read(result)
+		
+		# Opens specific file path.
+		elif option == 2:
+			# Asks the users for file path and checks if file path exsists.
+			while True:
 				try:
-					option = int(option)
-				except:
-					option = 1
-				if option == 1:
-					# End the checking loop.
-					pathChecking = False
-					print("\n") # Unnecessary formatting choice.
-					start_menu()
-	
-	# Stops the program.
-	elif option == 3:
-		exit()
-	
-	# Loops through the program again.
-	else:
-		print("That's an invalid option!\n") # Unnecessary formatting choice.
-		start_menu()	# TODO : Not the best way todo this should be looping the entire thing.
+					path = input('Surely! What is the path?\n')
+				
+				except ValueError or TypeError:
+					print('Please input file path in a valid format.')
+					continue
+				
+				# If the path is real.
+				if os.path.exsits(path):
+					# Read the path then break out of the loop.
+					reader.read(path)
+					break
+				
+				# If no path exsists.
+				else:
+					print('No such path exists!')
+
+					# Loop for asking if the user wishes to input a new file path.
+					while True:
+						try:
+							option = int(input('Try again? (1. No / 2. Yes)\n'))
+							if option == 1 or option == 2:
+								break
+
+							# If the user inputted a number that is not 1 or 2.
+							else:
+								print('Please enter a valid option.')
+								continue
+						
+						# If the value is not an int.
+						except ValueError:
+							print('Please enter a valid option.')
+							continue
+					
+					# Try Again?, No. break the loop.
+					if option == 1:
+						break
+					
+					# Try Again?, Yes. continue the loop.
+					if option == 2:
+						continue
+		
+		# Stops the program.
+		elif option == 3:
+			exit()
+		
+		# Loops through the program again.
+		else:
+			# Breaks the main loop which exits the program
+			print("That's an invalid option!\n") # Unnecessary formatting choice.
+			break
 
 
 if __name__ == '__main__':
